3c3d5caf69fa5d069e42f2ebf1fc90b3d9a555eb
leeloomulti <leeloomulti@gmail.com>
2019-12-07 13:00:56 +0100

    New interface design, construction of questions in scrollView with progressView

--- P12/Controller/QuizzViewController.swift
+++ P12/Controller/QuizzViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class QuizzViewController: UIViewController {
-    
+//    let progress = Progress(totalUnitCount: 5)
+ 
+    @IBOutlet weak var questionOneButtons: UIButton!
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
+    @IBOutlet weak var progressView: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        progress.completedUnitCount = 0
 //        scrollView.isPagingEnabled = true
 
         // Do any additional setup after loading the view.
@@ -36,8 +40,9 @@ class QuizzViewController: UIViewController {
 extension QuizzViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageWidth = scrollView.bounds.width
-        let pageFraction = scrollView.contentOffset.x/pageWidth
+        let pageFraction = (scrollView.contentOffset.x/pageWidth)
+        progressView.setProgress(Float(pageFraction/5+0.2), animated: false)
 
-        pageControl.currentPage = Int((round(pageFraction)))
+//        pageControl.currentPage = Int((round(pageFraction)))
     }
 }
